a7d78118b457c4265c433b22d16a7ad63a814e85
Max Ustinov <ewclide@yandex.ru>
2024-06-27 21:43:21 +0400

    update tile (add container & top padding)

--- src/tile.ts
+++ src/tile.ts
@@ -1,4 +1,4 @@
-import { Point, Sprite, Texture } from 'pixi.js';
+import { Container, Point, Sprite, Texture } from 'pixi.js';
 import { TileType } from './game';
 
 export interface TileOptions {
@@ -15,19 +15,30 @@ export class Tile {
     readonly id = tileID++;
     readonly type: TileType;
     readonly sprite: Sprite;
+    readonly container: Container;
+    readonly topPadding: number;
     speed: number = 0;
 
     constructor(options: TileOptions) {
         const { width, height, position, type, texture, zIndex } = options;
 
         const sizeFactor = texture.height / texture.width;
+        const topPadding = height * (sizeFactor - 1);
+
+        const container = new Container();
+        container.position.copyFrom(position);
+
         const sprite = new Sprite(texture);
-        sprite.position.copyFrom(position);
+        sprite.position.y -= topPadding;
         sprite.width = width;
         sprite.height = height * sizeFactor;
         sprite.zIndex = zIndex;
 
+        container.addChild(sprite);
+
         this.type = type;
         this.sprite = sprite;
+        this.container = container;
+        this.topPadding = topPadding;
     }
 }
