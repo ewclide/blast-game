5d3b76b32ea325439906d370e4b0126c90ad45e3
Max Ustinov <ewclide@yandex.ru>
2024-06-30 22:34:52 +0400

    add game assets config

---
+++ src/game/assets.ts
@@ -0,0 +1,23 @@
+import { ResourcesPixiConfig } from '../framework';
+
+export const assets: ResourcesPixiConfig = {
+    fonts: {
+        marvin: 'fonts/marvin.woff2',
+    },
+    textures: {
+        'tile-blue': 'images/tile-blue.png',
+        'tile-red': 'images/tile-red.png',
+        'tile-pink': 'images/tile-pink.png',
+        'tile-yellow': 'images/tile-yellow.png',
+        'tile-green': 'images/tile-green.png',
+        'grid-back': 'images/grid-back.png',
+        'scores-back': 'images/scores-back.png',
+        'booster-back': 'images/booster-back.png',
+        'progress-back': 'images/progress-back.png',
+        'progress-fill': 'images/progress-fill.png',
+        'progress-bg': 'images/progress-bg.png',
+        'pause-button': 'images/pause-button.png',
+        'pink-button': 'images/pink-button.png',
+        'red-button': 'images/red-button.png',
+    },
+};
