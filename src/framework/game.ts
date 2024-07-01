6684f7c650ebde926fa0fa15e1bb561ae545f56e
Max Ustinov <ewclide@yandex.ru>
2024-07-01 17:17:01 +0400

    refactor game interface (separate with scene manager)

--- src/framework/game.ts
+++ src/framework/game.ts
@@ -1,62 +1,27 @@
-import { Context } from './context';
 import { ResourcesPixiConfig } from './pixi';
-import { Scene } from './scene';
+import { ISceneManager } from './scene-manager';
+import { Context } from './context';
 
-export class Game {
-    private _scenes: Map<string, Scene> = new Map();
-    private _activeScene: Scene | null = null;
-    private _assets: ResourcesPixiConfig = {};
+export interface IGame {
+    init(): Promise<void>;
+    exit(): void;
+}
 
-    setAssets(assets: ResourcesPixiConfig) {
+export class BaseGame implements IGame {
+    protected _sceneManager: ISceneManager;
+    protected _assets: ResourcesPixiConfig;
+
+    constructor(sceneManager: ISceneManager, assets: ResourcesPixiConfig) {
+        this._sceneManager = sceneManager;
         this._assets = assets;
     }
 
     async init(): Promise<void> {
-        const { resources } = Context.get();
+        const { resources, pixi } = Context.get();
         await resources.load(this._assets);
-        console.log('after resouces load');
 
-        for (const scene of this._scenes.values()) {
-            await scene.init();
-        }
-
-        console.log(resources);
-
-        this.getActiveScene().start();
+        pixi.ticker.add(this._sceneManager.update);
     }
 
-    addScene(scene: Scene) {
-        this._scenes.set(scene.name, scene);
-    }
-
-    getScene(name: string): Scene {
-        const scene = this._scenes.get(name);
-        if (scene === undefined) {
-            throw new Error();
-        }
-
-        return scene;
-    }
-
-    setActiveScene(name: string) {
-        const scene = this.getScene(name);
-        this._activeScene = scene;
-    }
-
-    getActiveScene(): Scene {
-        const scene = this._activeScene;
-        if (scene === null) {
-            throw new Error();
-        }
-        return scene;
-    }
-
-    update = () => {
-        this.updateCrossSceneLogic();
-        if (this._activeScene) {
-            this._activeScene.update();
-        }
-    };
-
-    updateCrossSceneLogic() {}
+    exit(): void {}
 }
